000af8e015a4c32e32b305e8d6a524779cd63841
katsumatarin <nir.k021017@gmail.com>
2020-12-16 13:54:09 +0900

    作業

---
+++ src/chirakari_hantei.py
@@ -0,0 +1,51 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import os
+import capture_depth_image
+import save_frame_sec
+import detect_contour
+
+def chirakari_hantei():
+  print("出力ファイル名を入力してください")
+  output_name = input()
+  os.makedirs(f'./data/out/{output_name}')
+
+  output_video_file_name = f'./data/out/{output_name}/{output_name}.mp4'
+
+  # ビデオを撮影し、保存する
+  capture_depth_image.capture_depth_image(output_video_file_name)
+
+  while(True):
+    print(f"何秒目の部屋の散らかりをチェックしますか？終了する場合は-1を入力してください。")
+    sec = int(input())
+
+    if sec == -1:
+      break
+
+    output_image_file_name = f'./data/out/{output_name}/{output_name}_{sec}sec.jpg'
+
+    # 指定された秒数の画像を保存
+    save_frame_sec.save_frame_sec(output_video_file_name, sec, output_image_file_name)
+
+    detect_count, src = detect_contour.detect_contour(output_image_file_name)
+
+    if detect_count > 10:
+      print('部屋が汚れています！大至急片付けましょう！')
+    elif detect_count > 0:
+      print('部屋が少し汚いので片付けてください！')
+    else:
+      print('とても綺麗です！このまま継続しましょう！')
+
+    # 外接矩形された画像を表示
+    cv2.imshow('output', src)
+    cv2.waitKey(0)
+
+    # 終了処理
+    cv2.destroyAllWindows()
+
+
+  os.remove(output_video_file_name)
+
+if __name__ == '__main__':
+  chirakari_hantei()
---
+++ src/detect_contour.py
@@ -0,0 +1,54 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import os
+import capture_depth_image
+import save_frame_sec
+
+# 指定した画像(path)の物体を検出し、外接矩形の画像を出力。
+# 検出した物体の数と外接矩形の画像を返す。
+def detect_contour(path):
+
+  # 画像を読込
+  src = cv2.imread(path, cv2.IMREAD_COLOR)
+
+  # グレースケール画像へ変換
+  gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+  # 2値化
+  retval, bw = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+  # 輪郭を抽出
+  #   contours : [領域][Point No][0][x=0, y=1]
+  #   cv2.CHAIN_APPROX_NONE: 中間点も保持する
+  #   cv2.CHAIN_APPROX_SIMPLE: 中間点は保持しない
+  contours, hierarchy = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+  # 矩形検出された数（デフォルトで0を指定）
+  detect_count = 0
+
+  # 各輪郭に対する処理
+  for i in range(0, len(contours)):
+
+    # 輪郭の領域を計算
+    area = cv2.contourArea(contours[i])
+
+    # ノイズ（小さすぎる領域）と全体の輪郭（大きすぎる領域）を除外
+    if area < 1e2 or 1e5 < area:
+      continue
+
+    # 外接矩形
+    if len(contours[i]) > 0:
+      rect = contours[i]
+      x, y, w, h = cv2.boundingRect(rect)
+      cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+      # 外接矩形毎に画像を保存
+      path_dir_base, path_ext = os.path.splitext(path)
+      out_path = path_dir_base + '_detect' + path_ext
+
+      cv2.imwrite(out_path, src[y:y + h, x:x + w])
+
+      detect_count = detect_count + 1
+
+  return detect_contour, src
